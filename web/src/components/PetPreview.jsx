5eb3674cb22a63b8fdf489042c8f04da57b65038
SebastianHagemeyer <sebalhag@icloud.com>
2025-12-01 19:07:07 +1100

    refresh

--- web/src/components/PetPreview.jsx
+++ web/src/components/PetPreview.jsx
@@ -1,5 +1,5 @@
 // src/components/PetPreview.jsx
-//renamed
+//renamed test
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import PetThumbnailCanvas from "./PetThumbnailCanvas";
